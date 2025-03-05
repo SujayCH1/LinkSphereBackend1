f7bb460053bad674584c9271d3b53afd714311fe
SujayCH1 <suju.narayan@gmail.com>
2025-03-05 17:29:19 +0530

    commit 1

---
+++ Backend/smartConnect/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def smartConnectAlgorithm(request):
+    return HttpResponse("This AI based reccomdation system API")
---
+++ Backend/supabase.py
@@ -0,0 +1,13 @@
+import os
+from supabase import create_client, Client
+from env import 
+
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
+
+
+
+
+
+
